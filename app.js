7264b3c490688c4dc91ad31d02ae4adc35fb85b7
Punidk <advice39za@gmail.com>
2026-03-31 00:30:47 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -263,7 +263,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const aiMessages = document.getElementById('ai-messages');
 
     // Using our own AI Backend API
-    const AI_BACKEND_URL = "http://127.0.0.1:8000/api/chat";
+    const AI_BACKEND_URL = "https://ee32.onrender.com/api/chat";
 
     // Toggle Window
     aiFab.addEventListener('click', () => {
